c1ede73d0f8fedb17045b3233a1c2655a2101f88
bhavani <bhavanik123bhk@gmail.com>
2022-05-25 13:26:13 +0530

    import prg

---
+++ corejava/src/abcpackage/addition.java
@@ -0,0 +1,8 @@
+package abcpackage;
+
+public class addition {
+    public int addtwonumbers(int a,int b)
+    {
+    return a+b;
+    }
+}
---
+++ corejava/src/xyzpackage/test.java
@@ -0,0 +1,15 @@
+package xyzpackage;
+
+import abcpackage.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		addition obj=new addition();
+		obj.addtwonumbers(1,2);
+		System.out.println(obj.addtwonumbers(1, 2));
+
+	}
+
+}
