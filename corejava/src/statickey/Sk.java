b41de0f8ad32bfdf0068ce2e2be663b6970179b9
bhavani <bhavanik123bhk@gmail.com>
2022-05-30 14:18:08 +0530

    static keyword prg

---
+++ corejava/src/statickey/Sk.java
@@ -0,0 +1,17 @@
+package statickey;
+
+public class Sk {
+	static int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Sk ob1=new Sk();
+       Sk ob2=new Sk();
+       ob1.age=20;
+       ob2.age=40;
+       System.out.println("the age is:"+ob1.age);
+       System.out.println("the age is:"+ob2.age);
+       
+	}
+
+}
