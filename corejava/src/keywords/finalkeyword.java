a366db935cee76c40419bc30d0332560b86d4843
bhavani <bhavanik123bhk@gmail.com>
2022-06-07 22:05:10 +0530

    finalkey prg

---
+++ corejava/src/keywords/finalkeyword.java
@@ -0,0 +1,24 @@
+package keywords;
+class A{
+	 void run()
+	{
+		
+	}
+}
+public class finalkeyword {
+	int speedlimit=288;
+	
+	void speed()
+	{
+		speedlimit=200;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		finalkeyword ob=new finalkeyword();
+		System.out.println("the speedlimit is:");
+		System.out.println(ob.speedlimit);
+			}
+
+}
