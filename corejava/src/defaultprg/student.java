bb42181ce282bace7c213e8d9edf6dbfd49f1168
bhavani <bhavanik123bhk@gmail.com>
2022-06-07 22:15:24 +0530

    default prg

---
+++ corejava/src/defaultprg/student.java
@@ -0,0 +1,25 @@
+package defaultprg;
+
+
+ class student {
+	 student()
+	 {
+		 System.out.println("student object is created");
+	 }
+	 void commission()
+     {
+    	String  name;
+    	 int phno;
+    	 float sales_amt;
+    	 String addres;
+    	
+     }
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		student ob=new student();
+		//ob.student();
+		
+	}
+
+}
