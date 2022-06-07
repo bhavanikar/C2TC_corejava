1edd2814481676fa9f8b59012ad5b5382191c35d
bhavani <bhavanik123bhk@gmail.com>
2022-06-07 22:00:23 +0530

    demo prg

---
+++ corejava/src/demo/TryCatchDemop.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class TryCatchDemop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str=null;
+        try {
+        	str.equals("hello");
+        	
+        }
+        catch(NullPointerException ne) 
+        {
+        	str=new String("hello");
+        	System.out.println(str.equals("hello"));
+        }
+        System.out.println("continuing in the program");   
+	}
+
+}
