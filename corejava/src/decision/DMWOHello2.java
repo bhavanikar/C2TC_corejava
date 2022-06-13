bddd74a973c291bb4525371ba13a051aaef7925b
bhavani <bhavanik123bhk@gmail.com>
2022-06-13 21:03:58 +0530

    code nine prg

---
+++ corejava/src/decision/DMWOHello2.java
@@ -0,0 +1,18 @@
+package decision;
+
+public class DMWOHello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int x=8,y=7;
+       if(x>=y)
+       {
+    	   System.out.println("statement is true");
+       }
+       else
+       {
+    	   System.out.println("statment is false");
+       }
+	}
+
+}
