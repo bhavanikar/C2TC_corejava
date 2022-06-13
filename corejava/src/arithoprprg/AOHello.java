cd21a8aaf7ef949fcbbe077edf767fa71dad4f4f
bhavani <bhavanik123bhk@gmail.com>
2022-06-13 21:00:48 +0530

    code seven prg

---
+++ corejava/src/arithoprprg/AOHello.java
@@ -0,0 +1,20 @@
+package arithoprprg;
+
+public class AOHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int x=10,y=20;
+     System.out.println(x+y);
+     System.out.println(--x);
+     System.out.println(++y);
+     System.out.println(x-y);
+     System.out.println(--y);
+     System.out.println(x++);
+     System.out.println(++x);
+     System.out.println(y++);
+     System.out.println(x--);
+     System.out.println(y--);
+	}
+
+}
