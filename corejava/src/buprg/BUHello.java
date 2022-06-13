6711b8047842f4535337443a2733b5943c3168b7
bhavani <bhavanik123bhk@gmail.com>
2022-06-13 20:00:47 +0530

    code four prg

---
+++ corejava/src/buprg/BUHello.java
@@ -0,0 +1,54 @@
+package buprg;
+
+public class BUHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       boolean var=false;
+       System.out.println(var);
+       char var1='\u00A1';
+       System.out.println(var1);
+       char var2='\u00A2';
+       System.out.println(var2);
+       char var3='\u00A3';
+       System.out.println(var3);
+       char var4='\u00A4';
+       System.out.println(var4);
+       char var5='\u00A5';
+       System.out.println(var5);
+       char var6='\u00A6';
+       System.out.println(var6);
+       char var7='\u00A7';
+       System.out.println(var7);
+       char var8='\u00A8';
+       System.out.println(var8);
+       char var9='\u00A9';
+       System.out.println(var9);
+       char var10='\u00AA';
+       System.out.println(var10);
+       char var11='\u00AB';
+       System.out.println(var11);
+       char var12='\u00AC';
+       System.out.println(var12);
+       char var13='\u00AD';
+       System.out.println(var13);
+       char var14='\u00AE';
+       System.out.println(var14);
+       char var15='\u00AF';
+       System.out.println(var15);
+       char var16='\u00FF';
+       System.out.println(var16);
+       
+       char var17='\u00BB';
+       System.out.println(var17);
+       char var18='\u00CC';
+       System.out.println(var18);
+       char var19='\u00DD';
+       System.out.println(var19);
+       char var20='\u00EE';
+       System.out.println(var20);
+       char var21='\u00AA';
+       System.out.println(var21);
+	}
+
+}
