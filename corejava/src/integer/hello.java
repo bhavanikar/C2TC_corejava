dbc1de4556981d838a7793c5fea3af2df0563c65
bhavani <bhavanik123bhk@gmail.com>
2022-06-13 19:35:11 +0530

    code one prg

---
+++ corejava/src/integer/hello.java
@@ -0,0 +1,19 @@
+package integer;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int max=2147483647;
+       int min=-217483648;
+       short shortMax=32767;
+       short shortMin=-32768;
+       long longMax=9223372036854775807l;
+       long longMin=-9223372036854775808l;
+       byte byteMax=127;
+       byte byteMin=-128;
+       
+       System.out.println("");
+	}
+
+}
