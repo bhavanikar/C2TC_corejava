66f53b31fcc7315638798e9c5fb19cbd26531001
bhavani <bhavanik123bhk@gmail.com>
2022-06-13 20:36:58 +0530

    code five prg

---
+++ corejava/src/strprg/STRHello.java
@@ -0,0 +1,11 @@
+package strprg;
+
+public class STRHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String var=new String("hello world");
+        System.out.println(var);
+	}
+
+}
